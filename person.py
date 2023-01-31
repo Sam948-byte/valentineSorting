944bb9c3fc9134df26b2cdd766300f2e98bc08ab
Sam <techguy763@gmail.com>
2023-01-31 17:55:26 +0000

    specific person changes(actually staged)

--- person.py
+++ person.py
@@ -1,21 +1,23 @@
 #imports
 import os
+import sys
 
 names = os.listdir('answers')
 
-for x in names:
-    answersFile = open('answers/' + x.strip(), 'r')
-    answers = answersFile.readlines()
-    answersFile.close()
-    numQuestions = len(answers)
-    for y in names:
-        match = 0
-        if y == x:
-            break
-        compAnswersFile = open('answers/' + y.strip(), 'r')
-        compAnswers = compAnswersFile.readlines()
-        compAnswersFile.close()
-        for iteration, z in enumerate(answers, start=0):
-            if z.strip() == compAnswers[iteration].strip():
-                match += 1
-        print(x.strip('.txt') + ' has a ' + str(round(match / numQuestions * 100, 2)) + '% compatability with ' + y.strip('.txt'))
+
+answersFile = open('answers/' + sys.argv[1].strip() + '.txt', 'r')
+answers = answersFile.readlines()
+answersFile.close()
+numQuestions = len(answers)
+
+for y in names:
+    match = 0
+    if y.strip('.txt') == sys.argv[1]:
+        continue
+    compAnswersFile = open('answers/' + y.strip(), 'r')
+    compAnswers = compAnswersFile.readlines()
+    compAnswersFile.close()
+    for iteration, z in enumerate(answers, start=0):
+        if z.strip() == compAnswers[iteration].strip():
+            match += 1
+    print(str(sys.argv[1]) + ' has a ' + str(round(match / numQuestions * 100, 2)) + '% compatability with ' + y.strip('.txt'))
